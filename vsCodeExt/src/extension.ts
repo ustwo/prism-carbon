042be25f07b7cfbeeba71b0a9c8ae453b3734e97
Jacob Connor <gn24034@bristol.ac.uk>
2025-10-24 12:46:14 +0100

    Update vsCodeExt/src/extension.ts
    Co-authored-by: Iman Hadi <143740994+iman-ui@users.noreply.github.com>

--- vsCodeExt/src/extension.ts
+++ vsCodeExt/src/extension.ts
@@ -6,7 +6,6 @@ import * as vscode from 'vscode';
 import * as https from 'https';
 
 // This method is called when your extension is activated
-
 // Your extension is activated the very first time the command is executed
 
 export function activate(context: vscode.ExtensionContext) {
