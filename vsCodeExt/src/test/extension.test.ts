6a1335bf374cedbcfd79b27a88741256e370bc9a
Max <cg24012@bristol.ac.uk>
2026-02-01 01:30:09 +0000

    tests all written commands and ensures they don't crash

--- vsCodeExt/src/test/extension.test.ts
+++ vsCodeExt/src/test/extension.test.ts
@@ -5,6 +5,7 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 import * as extension from '../extension';
 import { isFunctionDeclaration } from 'typescript';
+import * as sinon from 'sinon';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -15,7 +16,7 @@ suite('Extension Test Suite', () => {
 	});
 });
 
-suite('Commands are registered', () => {
+suite('CommandTests', () => {
 	// gets all registered commands
 	let allCommands: string[];
 	setup(async () => {
@@ -41,6 +42,23 @@ suite('Commands are registered', () => {
 	test('openDashboard exists and runs', () => {
 		const found = allCommands.indexOf('vsCodeExt.openDashboard') > -1;
 		assert.strictEqual(found, true, 'Command was not registered!');
+	});
+
+	test('All written commands execute without crashing', async () => {
+		const stubInput = sinon.stub(vscode.window, 'showInputBox').resolves('50');
+		const myExtensionCommands = allCommands.filter(cmd => cmd.startsWith('vsCodeExt.'));
+		try {
+			for (const command of myExtensionCommands) {
+				console.log(`Running: ${command}`);
+
+				await vscode.commands.executeCommand(command);
+			}
+		} catch (error) {
+			assert.fail(`Command failed to execute: ${error}`);
+		}
+		finally {
+			stubInput.restore();
+		}
 	});
 	// below test is for checking failed tests fail. They do!
 
