55ca2156210fe9f4630aa089364673eab5ed54a0
Max <cg24012@bristol.ac.uk>
2026-02-18 10:47:11 +0000

    Updated convert file to correctly identify claude haiku 4.5 instead of 5

--- vsCodeExt/src/convert.ts
+++ vsCodeExt/src/convert.ts
@@ -36,7 +36,7 @@ export const modelRegistry: Record<string, TieredModel> = {
     "gpt-4o": new TieredModel("GPT4o", [{ limit: 400, carbonPerToken: 0.000000370125 }, { limit: 2000, carbonPerToken: 0.000000212625 }, { limit: 11500, carbonPerToken: 0.0000000875 }]),
     "gpt-4.5": new TieredModel("GPT4.5", [{ limit: veryLarge, carbonPerToken: 0.0003 }]),
     "gpt-5": new TieredModel("GPT5", [{ limit: veryLarge, carbonPerToken: 0.00269722222 }]), // ESTIMATED //https://impact.esg.ai/
-    "claude-haiku-5": new TieredModel("ClaudeHaiku5", [{ limit: veryLarge, carbonPerToken: 0.000269444444 }]),  //https://impact.esg.ai/
+    "claude-haiku-4.5": new TieredModel("ClaudeHaiku5", [{ limit: veryLarge, carbonPerToken: 0.000269444444 }]),  //https://impact.esg.ai/
     "claude-sonnet-4.5": new TieredModel("ClaudeSonnet4.5", [{ limit: veryLarge, carbonPerToken: 0.0005388888889 }]),  //https://impact.esg.ai/
     "claude-opus-4.5": new TieredModel("ClaudeOpus4.5", [{ limit: veryLarge, carbonPerToken: 0.0561888888888889 }]),  //https://impact.esg.ai/
     "claude": new TieredModel("ClaudeOpus4.5", [{ limit: veryLarge, carbonPerToken: 0.000269444444 }]), // generic claude catcher
