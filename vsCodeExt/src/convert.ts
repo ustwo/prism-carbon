b6d049e11f5ec5558f9310114ea54d50d16936c3
m0g56 <138621757+m0g56@users.noreply.github.com>
2026-04-22 15:08:12 +0100

    Increase larger prompt limit

--- vsCodeExt/src/convert.ts
+++ vsCodeExt/src/convert.ts
@@ -49,66 +49,66 @@ const veryLarge = Number.MAX_SAFE_INTEGER;
 //https://app.powerbi.com/view?r=eyJrIjoiZjVmOTI0MmMtY2U2Mi00ZTE2LTk2MGYtY2ZjNDMzODZkMjlmIiwidCI6IjQyNmQyYThkLTljY2QtNDI1NS04OTNkLTA2ODZhMzJjMTY4ZCIsImMiOjF9
 
 export const modelRegistry: Record<string, TieredModel> = {
-    "o4-mini-high": new TieredModel("OpenAI o4-mini (high)", [{limit: 2000, energyPerToken: 6.13/2000}, { limit: 11500, energyPerToken: 5.66/11500 }]),
-    "o3-pro": new TieredModel("OpenAI o3 Pro", [{limit: 2000, energyPerToken: 32.36/2000}, { limit: 11500, energyPerToken: 36.08/11500 }]),
-    "o3-mini-high": new TieredModel("OpenAI o3 Mini (high)", [{ limit: 2000, energyPerToken: 5.04/2000 }, { limit: 11500, energyPerToken: 8.47/11500 }]),
-    "o3-mini": new TieredModel("OpenAI o3 Mini", [{ limit: 2000, energyPerToken: 1.72/2000 }, { limit: 11500, energyPerToken: 2.7/11500 }]),
-    "o3": new TieredModel("OpenAI o3", [{limit: 2000, energyPerToken: 4.32/2000}, { limit: 11500, energyPerToken: 5.73/11500 }]),
-    "o1": new TieredModel("OpenAI o1", [{ limit: 2000, energyPerToken: 7.03/2000 }, { limit: 11500, energyPerToken: 14.4/11500 }]),
+    "o4-mini-high": new TieredModel("OpenAI o4-mini (high)", [{limit: 2000, energyPerToken: 6.13/2000}, { limit: veryLarge, energyPerToken: 5.66/11500 }]),
+    "o3-pro": new TieredModel("OpenAI o3 Pro", [{limit: 2000, energyPerToken: 32.36/2000}, { limit: veryLarge, energyPerToken: 36.08/11500 }]),
+    "o3-mini-high": new TieredModel("OpenAI o3 Mini (high)", [{ limit: 2000, energyPerToken: 5.04/2000 }, { limit: veryLarge, energyPerToken: 8.47/11500 }]),
+    "o3-mini": new TieredModel("OpenAI o3 Mini", [{ limit: 2000, energyPerToken: 1.72/2000 }, { limit: veryLarge, energyPerToken: 2.7/11500 }]),
+    "o3": new TieredModel("OpenAI o3", [{limit: 2000, energyPerToken: 4.32/2000}, { limit: veryLarge, energyPerToken: 5.73/11500 }]),
+    "o1": new TieredModel("OpenAI o1", [{ limit: 2000, energyPerToken: 7.03/2000 }, { limit: veryLarge, energyPerToken: 14.4/11500 }]),
 
 
 
 
-    "gpt-5-mini-high": new TieredModel("GPT5 Mini (high)", [{ limit: 2000, energyPerToken: 14.86/2000 }, { limit: 11500, energyPerToken: 13.37/11500 }]),
-    "gpt-5-mini-medium": new TieredModel("GPT5 Mini (medium)", [{ limit: 2000, energyPerToken: 4.3/2000 }, { limit: 11500, energyPerToken: 3.52/11500 }]),
-    "gpt-5-nano-high": new TieredModel("GPT5 Nano (high)", [{ limit: 2000, energyPerToken: 6.65/2000 }, { limit: 11500, energyPerToken: 6.45/11500 }]),
-    "gpt-5-nano-medium": new TieredModel("GPT5 Nano (medium)", [{ limit: 2000, energyPerToken: 3.87/2000 }, { limit: 11500, energyPerToken: 3.14/11500 }]),
-    "gpt-5-nano-minimal": new TieredModel("GPT5 Nano (minimal)", [{ limit: 2000, energyPerToken: 0.5/2000 }, { limit: 11500, energyPerToken: 0.65/11500 }]),
-    "gpt-5-minimal": new TieredModel("GPT5 (minimal)", [{ limit: 2000, energyPerToken: 3.00/2000 }, { limit: 11500, energyPerToken: 4.72/11500 }]),
-    "gpt-5-mini": new TieredModel("GPT5 Mini", [{ limit: 2000, energyPerToken: 4.30/2000 }, { limit: 11500, energyPerToken: 3.52/11500}]),
+    "gpt-5-mini-high": new TieredModel("GPT5 Mini (high)", [{ limit: 2000, energyPerToken: 14.86/2000 }, { limit: veryLarge, energyPerToken: 13.37/11500 }]),
+    "gpt-5-mini-medium": new TieredModel("GPT5 Mini (medium)", [{ limit: 2000, energyPerToken: 4.3/2000 }, { limit: veryLarge, energyPerToken: 3.52/11500 }]),
+    "gpt-5-nano-high": new TieredModel("GPT5 Nano (high)", [{ limit: 2000, energyPerToken: 6.65/2000 }, { limit: veryLarge, energyPerToken: 6.45/11500 }]),
+    "gpt-5-nano-medium": new TieredModel("GPT5 Nano (medium)", [{ limit: 2000, energyPerToken: 3.87/2000 }, { limit: veryLarge, energyPerToken: 3.14/11500 }]),
+    "gpt-5-nano-minimal": new TieredModel("GPT5 Nano (minimal)", [{ limit: 2000, energyPerToken: 0.5/2000 }, { limit: veryLarge, energyPerToken: 0.65/11500 }]),
+    "gpt-5-minimal": new TieredModel("GPT5 (minimal)", [{ limit: 2000, energyPerToken: 3.00/2000 }, { limit: veryLarge, energyPerToken: 4.72/11500 }]),
+    "gpt-5-mini": new TieredModel("GPT5 Mini", [{ limit: 2000, energyPerToken: 4.30/2000 }, { limit: veryLarge, energyPerToken: 3.52/11500}]),
 
-    "gpt-5-high": new TieredModel("GPT5 (high)", [{limit: 2000, energyPerToken: 22.16/2000}, { limit: 11500, energyPerToken: 21.83/11500 }]),
-    "gpt-5-medium": new TieredModel("GPT5 (medium)", [{limit: 2000, energyPerToken: 11.89/2000}, { limit: 11500, energyPerToken: 10.59/11500 }]),
-    "gpt-5-low": new TieredModel("GPT5 (low)", [{limit: 2000, energyPerToken: 5.01/2000}, { limit: 11500, energyPerToken: 6.47/11500 }]),
-    "gpt-5": new TieredModel("GPT5 (medium)", [{limit: 2000, energyPerToken: 11.89/2000}, { limit: 11500, energyPerToken: 10.59/11500 }]),
+    "gpt-5-high": new TieredModel("GPT5 (high)", [{limit: 2000, energyPerToken: 22.16/2000}, { limit: veryLarge, energyPerToken: 21.83/11500 }]),
+    "gpt-5-medium": new TieredModel("GPT5 (medium)", [{limit: 2000, energyPerToken: 11.89/2000}, { limit: veryLarge, energyPerToken: 10.59/11500 }]),
+    "gpt-5-low": new TieredModel("GPT5 (low)", [{limit: 2000, energyPerToken: 5.01/2000}, { limit: veryLarge, energyPerToken: 6.47/11500 }]),
+    "gpt-5": new TieredModel("GPT5 (medium)", [{limit: 2000, energyPerToken: 11.89/2000}, { limit: veryLarge, energyPerToken: 10.59/11500 }]),
 
 
-    "gpt-4-turbo": new TieredModel("GPT4 Turbo", [{limit: 2000, energyPerToken: 7.01/2000}, { limit: 11500, energyPerToken: 10.93/11500 }]),
-    "gpt-4.1-nano": new TieredModel("GPT4.1 Nano", [{ limit: 2000, energyPerToken: 0.36/2000 }, { limit: 11500, energyPerToken: 0.57/11500 }]),
-    "gpt-4.1": new TieredModel("GPT4.1", [{limit: 2000, energyPerToken: 1.85/2000}, { limit: 11500, energyPerToken: 2.94/11500 }]),
-    "gpt-4o-2024-11": new TieredModel("GPT4o November", [{ limit: 2000, energyPerToken: 1.33/2000 }, { limit: 11500, energyPerToken: 3.19/11500 }]),
-    "gpt-4o-2024-08": new TieredModel("GPT4o August", [{ limit: 2000, energyPerToken: 1.63/2000 }, { limit: 11500, energyPerToken: 2.24/11500 }]),
-    "gpt-4o-2024-05": new TieredModel("GPT4o May", [{ limit: 2000, energyPerToken: 2.18/2000 }, { limit: 11500, energyPerToken: 3.89/11500}]),
-    "gpt-4o-2024-03": new TieredModel("GPT4o March", [{ limit: 2000, energyPerToken: 1.18/2000 }, { limit: 11500, energyPerToken: 5.81/11500}]),
-    "gpt-4o-mini": new TieredModel("GPT4o mini", [{limit: 2000, energyPerToken: 1.65/2000}, { limit: 11500, energyPerToken: 3.85/11500 }]),
-    "gpt-4o": new TieredModel("GPT4o", [{ limit: 2000, energyPerToken: 1.30/2000 }, { limit: 11500, energyPerToken: 3.47/11500}]),
+    "gpt-4-turbo": new TieredModel("GPT4 Turbo", [{limit: 2000, energyPerToken: 7.01/2000}, { limit: veryLarge, energyPerToken: 10.93/11500 }]),
+    "gpt-4.1-nano": new TieredModel("GPT4.1 Nano", [{ limit: 2000, energyPerToken: 0.36/2000 }, { limit: veryLarge, energyPerToken: 0.57/11500 }]),
+    "gpt-4.1": new TieredModel("GPT4.1", [{limit: 2000, energyPerToken: 1.85/2000}, { limit: veryLarge, energyPerToken: 2.94/11500 }]),
+    "gpt-4o-2024-11": new TieredModel("GPT4o November", [{ limit: 2000, energyPerToken: 1.33/2000 }, { limit: veryLarge, energyPerToken: 3.19/11500 }]),
+    "gpt-4o-2024-08": new TieredModel("GPT4o August", [{ limit: 2000, energyPerToken: 1.63/2000 }, { limit: veryLarge, energyPerToken: 2.24/11500 }]),
+    "gpt-4o-2024-05": new TieredModel("GPT4o May", [{ limit: 2000, energyPerToken: 2.18/2000 }, { limit: veryLarge, energyPerToken: 3.89/11500}]),
+    "gpt-4o-2024-03": new TieredModel("GPT4o March", [{ limit: 2000, energyPerToken: 1.18/2000 }, { limit: veryLarge, energyPerToken: 5.81/11500}]),
+    "gpt-4o-mini": new TieredModel("GPT4o mini", [{limit: 2000, energyPerToken: 1.65/2000}, { limit: veryLarge, energyPerToken: 3.85/11500 }]),
+    "gpt-4o": new TieredModel("GPT4o", [{ limit: 2000, energyPerToken: 1.30/2000 }, { limit: veryLarge, energyPerToken: 3.47/11500}]),
     
     
-    "claude-haiku-4.5": new TieredModel("Claude Haiku 4.5", [{ limit: 2000, energyPerToken: 4.75/2000 }, { limit: 11500, energyPerToken: 8.73/11500 }]),
-    "claude-opus-4.1": new TieredModel("Claude Opus 4.1", [{ limit: 2000, energyPerToken: 6.97/2000 }, { limit: 11500, energyPerToken: 13.22/11500 }]),
-    "claude-sonnet-4.5": new TieredModel("Claude Sonnet 4.5", [{ limit: 2000, energyPerToken: 6.44/2000 }, { limit: 11500, energyPerToken: 10.11/11500 }]),
-    "claude-sonnet-4": new TieredModel("Claude Sonnet 4", [{ limit: 2000, energyPerToken: 5.63/2000 }, { limit: 11500, energyPerToken: 10.02/11500 }]),
-    "claude-opus-4": new TieredModel("Claude Opus 4", [{ limit: 2000, energyPerToken: 5.62/2000 }, { limit: 11500, energyPerToken: 10.73/11500 }]),
-    "claude-haiku-3": new TieredModel("Claude Haiku 3", [{ limit: 2000, energyPerToken: 1.77/2000 }, { limit: 11500, energyPerToken: 2.74/11500 }]),
-    "claude-sonnet": new TieredModel("Claude Sonnet (Generic - using 4.5 data)", [{ limit: 2000, energyPerToken: 6.44/2000 }, { limit: 11500, energyPerToken: 10.11/11500 }]),
-    "claude-haiku": new TieredModel("Claude Haiku (Generic - using 4.5 data)", [{ limit: 2000, energyPerToken: 4.75/2000 }, { limit: 11500, energyPerToken: 8.73/11500 }]),
-    "claude-opus": new TieredModel("Claude Opus (Generic - using 4.1 data)", [{ limit: 2000, energyPerToken: 6.97/2000 }, { limit: 11500, energyPerToken: 13.22/11500 }]),
+    "claude-haiku-4.5": new TieredModel("Claude Haiku 4.5", [{ limit: 2000, energyPerToken: 4.75/2000 }, { limit: veryLarge, energyPerToken: 8.73/11500 }]),
+    "claude-opus-4.1": new TieredModel("Claude Opus 4.1", [{ limit: 2000, energyPerToken: 6.97/2000 }, { limit: veryLarge, energyPerToken: 13.22/11500 }]),
+    "claude-sonnet-4.5": new TieredModel("Claude Sonnet 4.5", [{ limit: 2000, energyPerToken: 6.44/2000 }, { limit: veryLarge, energyPerToken: 10.11/11500 }]),
+    "claude-sonnet-4": new TieredModel("Claude Sonnet 4", [{ limit: 2000, energyPerToken: 5.63/2000 }, { limit: veryLarge, energyPerToken: 10.02/11500 }]),
+    "claude-opus-4": new TieredModel("Claude Opus 4", [{ limit: 2000, energyPerToken: 5.62/2000 }, { limit: veryLarge, energyPerToken: 10.73/11500 }]),
+    "claude-haiku-3": new TieredModel("Claude Haiku 3", [{ limit: 2000, energyPerToken: 1.77/2000 }, { limit: veryLarge, energyPerToken: 2.74/11500 }]),
+    "claude-sonnet": new TieredModel("Claude Sonnet (Generic - using 4.5 data)", [{ limit: 2000, energyPerToken: 6.44/2000 }, { limit: veryLarge, energyPerToken: 10.11/11500 }]),
+    "claude-haiku": new TieredModel("Claude Haiku (Generic - using 4.5 data)", [{ limit: 2000, energyPerToken: 4.75/2000 }, { limit: veryLarge, energyPerToken: 8.73/11500 }]),
+    "claude-opus": new TieredModel("Claude Opus (Generic - using 4.1 data)", [{ limit: 2000, energyPerToken: 6.97/2000 }, { limit: veryLarge, energyPerToken: 13.22/11500 }]),
 
 
-    "gemini-2.5-pro": new TieredModel("Gemini 2.5 Pro", [{ limit: 2000, energyPerToken: 2.29/2000 }, { limit: 11500, energyPerToken: 2.54/11500 }]),
-    "gemini-2.5-flash": new TieredModel("Gemini 2.5 Flash", [{ limit: 2000, energyPerToken: 1.24/2000 }, { limit: 11500, energyPerToken: 2.51/11500 }]),
+    "gemini-2.5-pro": new TieredModel("Gemini 2.5 Pro", [{ limit: 2000, energyPerToken: 2.29/2000 }, { limit: veryLarge, energyPerToken: 2.54/11500 }]),
+    "gemini-2.5-flash": new TieredModel("Gemini 2.5 Flash", [{ limit: 2000, energyPerToken: 1.24/2000 }, { limit: veryLarge, energyPerToken: 2.51/11500 }]),
     
     // data from this website since other study had no data for gemini models 3+
     // https://www.climatealigned.co/tools/ai-footprint-calculator
 
-    "gemini-3.1-pro": new TieredModel("Gemini 3.1 Pro", [{ limit: 2000, energyPerToken: 1.44/2000 }, { limit: 11500, energyPerToken: 10.33/11500 }]),
-    "gemini-3-flash": new TieredModel("Gemini 3 Flash", [{ limit: 2000, energyPerToken: 0.57/2000 }, { limit: 11500, energyPerToken: 4.14/11500 }]),
+    "gemini-3.1-pro": new TieredModel("Gemini 3.1 Pro", [{ limit: 2000, energyPerToken: 1.44/2000 }, { limit: veryLarge, energyPerToken: 10.33/11500 }]),
+    "gemini-3-flash": new TieredModel("Gemini 3 Flash", [{ limit: 2000, energyPerToken: 0.57/2000 }, { limit: veryLarge, energyPerToken: 4.14/11500 }]),
 
     
     
     // OLD DATA FROM BEFORE - IMPLEMENT NEW TIERS ABOVE
 
-    // // "gpt-4o-mini": new TieredModel("GPT4oMini", [{ limit: 400, energyPerToken: 0.00923 }, { limit: 2000, energyPerToken: 0.00369 }, { limit: 11500, energyPerToken: 0.0006293 }]),
+    // // "gpt-4o-mini": new TieredModel("GPT4oMini", [{ limit: 400, energyPerToken: 0.00923 }, { limit: 2000, energyPerToken: 0.00369 }, { limit: veryLarge, energyPerToken: 0.0006293 }]),
     // // "gpt-3.5-turbo": new TieredModel("GPT3.5Turbo", [{ limit: veryLarge, energyPerToken: 0.000002 }]),// kwh (1)
     // // "gpt-4-turbo": new TieredModel("GPT4Turbo", [{ limit: veryLarge, energyPerToken: 0.000006 }]), //kwh (1)
     // "gpt-4o": new TieredModel("GPT4o", [{ limit: veryLarge, energyPerToken: 0.9/365000 }]), //kwh (1)
